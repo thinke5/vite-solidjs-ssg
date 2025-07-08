2f675ada4a4a80761defa6b506918d133d2a8a0f
thinkeW <863093879@qq.com>
2025-07-08 14:24:47 +0800

    完善多语言

--- src/lib/i18n/index.tsx
+++ src/lib/i18n/index.tsx
@@ -4,14 +4,18 @@ import Cookie from 'js-cookie'
 import { createRoot, createSignal, startTransition } from 'solid-js'
 import { isServer } from 'solid-js/web'
 
+// ! 注意⚠️ 如果要改路径，下面也要改
 const allLangJson = import.meta.glob('../../locales/**/*.json', { import: 'default' }) // 自动导入，文档： https://cn.vite.dev/guide/features.html#glob-import
 
 // 提取出所有语言的命名空间
 const namespaces = Array.from(new Set(Object.keys(allLangJson).map(path => path.split('/')[4].split('.')[0])))
 
+// 提取出所有语言
+const allLangs = Array.from(new Set(Object.keys(allLangJson).map(path => path.split('/')[3].split('.')[0])))
+
 export const i18next = createInstance()
 // 默认语言
-const fallbackLng = 'zh'
+const fallbackLng = 'en'
 
 const { lang, setLang } = createRoot(() => {
   const [lang, setLang] = createSignal<string | null>(fallbackLng)
@@ -22,7 +26,12 @@ let isInit = false
 /** 初始化i18next */
 export async function initI18next() {
   const cookieLang = isServer ? null : Cookie.get('lang') // 获取cookie中的语言
-  const newLang = cookieLang || fallbackLng
+  let newLang = cookieLang || fallbackLng
+  // 回退语言
+  if (!allLangs.includes(newLang)) {
+    console.error('[ initI18next ]-->', `"${newLang}"语言不存在`)
+    newLang = fallbackLng
+  }
   if (!isInit) {
     isInit = true
     await i18next
@@ -47,6 +56,10 @@ export async function initI18next() {
 
 /** 切换语言 */
 export async function changeLanguage(newLang: string) {
+  if (!allLangs.includes(newLang)) {
+    console.error('[ i18n changeLanguage ]-->', `"${newLang}"语言不存在`)
+    newLang = fallbackLng
+  }
   await i18next.changeLanguage(newLang)
   await startTransition(() => {
     setLang(newLang)
